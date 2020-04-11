865443e40a61f301aaa06d21811709969227fba1
Joe Cox <joecox@live.co.uk>
2020-04-11 19:50:28 +0100

    Removed AJAX call due to issue with headers

--- js/functions.js
+++ js/functions.js
@@ -18,23 +18,96 @@ export function hideProjects() {
 
 // const cors = "https://cors-anywhere.herokuapp.com/";
 
-export function loadIndividualProject(page) {
-  $.ajax({
-    type: "POST",
-    url: `https://www.coejox.com/${page}.html`,
-    datatype: "html",
-    success: function (data, status) {
-      console.log(data);
-      console.log(status);
-      $(".page").html($(data));
-    },
-    error: function () {
-      console.info("Error");
-    },
-    done: function () {
-      console.info("DONE");
-    },
-  });
+// export function loadIndividualProject(page) {
+//   $.ajax({
+//     type: "POST",
+//     url: `https://www.coejox.com/${page}.html`,
+//     datatype: "html",
+//     success: function (data, status) {
+//       console.log(data);
+//       console.log(status);
+//       $(".page").html($(data));
+//     },
+//     error: function () {
+//       console.info("Error");
+//     },
+//     done: function () {
+//       console.info("DONE");
+//     },
+//   });
+// }
+
+export function insertProjectInfo(
+  image,
+  title,
+  information,
+  description,
+  link,
+  github
+) {
+  return `
+  <section class="section">
+  <div class="container">
+    <div class="level">
+      <div class="level-left">
+        <div class="pageinfo">
+          <h2 class="title is-1">${title}</h2>
+        </div>
+      </div>
+      <div class="level-right">
+        <div class="buttons is-grouped">
+          <button class="button is-primary orange project-back-btn">
+            <i class="fa fa-chevron-left"></i>
+          </button>
+
+          <a
+            class="button is-warning has-text-white"
+            href="mailto:joecoxwebdev@outlook.co.uk"
+          >
+            <i class="fa fa-envelope"></i>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <hr />
+  <div class="container project-container">
+    <div class="columns">
+      <div class="column is-half">
+        <figure class="image">
+          <img
+            src="${image}"
+            alt="${description}"
+          />
+        </figure>
+      </div>
+      <div class="column is-half">
+        <div class="buttons project-buttons">
+          <a
+            href="${link}"
+            class="button is-primary orange"
+            target="_blank"
+            >View Site</a
+          >
+          <a href="${github}" class="button is-primary" target="_blank"
+            ><i class="fab fa-github"></i>View on Github</a
+          >
+        </div>
+
+        <div class="tags are-small">
+          <span class="tag is-dark">Website</span>
+          <span class="tag is-dark">jQuery</span>
+          <span class="tag is-dark">Bootstrap</span>
+        </div>
+        <p class="content is-large">
+          ${information}
+        </p>
+      </div>
+    </div>
+  </div>
+</section>
+
+  `;
 }
 
 export function clearProject() {
